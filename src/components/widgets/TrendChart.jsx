67b82959b29f47f229709c9d519155020ebcfb4e
CodeNeBo <nechitaylob@gmail.com>
2023-09-11 15:56:04 +0300

    Bug fix. Downgraded SVGO dependencies for build. SVGO 3.0.2 doesn't work

---
+++ src/components/widgets/TrendChart.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const LineChart = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        label: 'Line 1',
+        data: [10, 20, 15, 30, 25, 40], // Replace with your data for Line 1
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      },
+      {
+        label: 'Line 2',
+        data: [5, 15, 10, 25, 20, 35], // Replace with your data for Line 2
+        borderColor: 'rgba(255, 99, 132, 1)',
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        beginAtZero: true,
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default LineChart;
