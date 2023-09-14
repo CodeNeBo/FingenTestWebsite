64c16e5db4c5949c3e52d3e2e66b4ec9a1fcaf40
CodeNeBo <nechitaylob@gmail.com>
2023-09-14 17:45:36 +0300

    TotalObs widget rework

---
+++ src/components/widgets/DynamicNum.jsx
@@ -0,0 +1,17 @@
+const number = {
+    value: '7,541,390',
+  };
+
+function DynamicNumber() {
+    return (
+        <h1 className="text-4xl">
+            {number.value}
+        </h1>
+    )
+}
+
+  export default function TotalObservations() {
+    return (
+        <DynamicNumber />
+    );
+  }
