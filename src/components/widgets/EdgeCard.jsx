fe33e272c3e7b0e5591f8d71c9684b7bc72db799
CodeNeBo <nechitaylob@gmail.com>
2023-09-29 14:25:08 +0300

    Dynamic Edge Cards

---
+++ src/components/widgets/EdgeCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const EdgeCard = ({ data }) => {
+  const sortedData = data.slice().sort((a, b) => b.number - a.number);
+
+  const formatNum = (number) => {
+        return number.toLocaleString('en-US');
+      };
+
+  return (
+    <div className="flex justify-start gap-3 ml-4 pr-4 overflow-x-scroll no-scrollbar">
+      {sortedData.map((item, index) => (
+        <div key={index} className="text-center">
+          <div
+            className={`rounded-3xl aspect-square w-32 h-32 flex justify-center items-center relative ${
+              item.number > 0
+                ? 'bg-gradient-to-tr from-accent to-gradient'
+                : 'bg-gradient-to-tr from-accent to-bluegradient'
+            }`}
+          >
+            <div className='bg-primary opacity-75 w-[95%] h-[95%] rounded-[20px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
+            <div className='z-50'>
+                <div className='flex flex-col items-center gap-0'>
+                    <h1 className="text-2xl font-semibold tracking-wide">{formatNum(item.number)}</h1>
+                    <p className='uppercase text-[10px] tracking-wider -translate-y-0.5'>edge</p>
+                </div>
+                <h4 className="text-sm mt-2 tracking-wider uppercase translate-y-4">{item.name}</h4>
+            </div>
+            
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EdgeCard;
---
+++ src/components/widgets/EdgeCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import EdgeCard from '~/components/widgets/EdgeCard.jsx';
+
+const ParentComponent = () => {
+  const data = [
+    { number: 1421, name: 'EUR / USD' },
+    { number: -32, name: 'BTC / USD' },
+    { number: 5590, name: 'USD / YEN' },
+    { number: -7232, name: 'ETH / USD' },
+    { number: 10012, name: 'GPB / USD' },
+  ];
+
+  return (
+    <div className='bg-primary'>
+      <EdgeCard data={data} />
+    </div>
+  );
+};
+
+export default ParentComponent;
