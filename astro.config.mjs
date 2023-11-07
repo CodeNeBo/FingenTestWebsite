7b17fc6ad0fc5e2950a21f4d8f88caa1472c363d
CodeNeBo <nechitaylob@gmail.com>
2023-11-07 15:00:19 +0200

    Blog path fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,6 +53,6 @@ export default defineConfig({
   },
   routes: {
     '/': 'src/pages/index.astro',
-    '[...slug]': 'src/pages/[...slug].astro',
+    'blog/[...slug]': 'src/pages/blog/[...slug].astro',
   }
 });
