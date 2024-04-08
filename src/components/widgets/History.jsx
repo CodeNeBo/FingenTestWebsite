597e9c53f58a7bbcfc2d4031ae4dd27658578ee4
Богдан Нечитайло <nechitaylob@gmail.com>
2024-04-08 21:19:12 +0300

    Improved dashboard borders

--- src/components/widgets/History.jsx
+++ src/components/widgets/History.jsx
@@ -13,7 +13,7 @@ const RecentSales = () => {
   const loadIcon = (currency) => {
     return (
       <img
-        src={`./src/icons/Crypto/${currency}.svg`}
+        src={`../src/icons/Crypto/${currency}.svg`}
         alt={`${currency} icon`}
         onError={(e) => {
           e.target.style.display = 'none';
@@ -60,49 +60,51 @@ const RecentSales = () => {
   );
 
   return (
-    <div className='bg-primary border border-white/5 backdrop-blur-xl rounded-lg text-textcolor p-4 md:p-6 flex-grow relative overflow-clip'>
-      <div class="absolute w-96 h-32 bg-accent blur-3xl rounded-full -z-10 opacity-20 -top-20 left-1/2 transform -translate-x-1/2"></div>
-      <div class="absolute w-32 h-32 bg-accent blur-3xl rounded-full -z-10 opacity-20 -bottom-20 left-0"></div>
-      <h2 className="md:text-sm tracking-wide pb-4 font-main">Recent Trades</h2>
-      <div className='h-64 overflow-y-scroll no-scrollbar'>
-        <ul className="divide-y divide-gray-300/5">
-          {salesData.map((trade, index) => (
-            <li key={index} className="py-4 flex items-center justify-between">
-              <div className="flex flex-row gap-1 items-center">
-                <div className='w-fit h-fit rounded-full mr-2 bg-white'>{loadIcon(trade.tradeCurrency1)}</div>
-                <p className="text-textcolor">
-                  {trade.tradeCurrency1}
-                </p>
-              </div>
-              <div className="flex flex-col">
-                <p className="text-right">
-                  {trade.wonBet >= 0 ? (
-                    <span>+${Math.abs(trade.wonBet)}</span>
-                  ) : (
-                    <span>-${Math.abs(trade.wonBet)}</span>
-                  )}
-                </p>
-                <div
-                  className={`text-white text-xs font-normal px-2 py-0.5 rounded mt-1 flex flex-row gap-1 items-center justify-center ${
-                    trade.wonBet >= 0 ? 'bg-[#3644DF]' : 'bg-[#EA3382]'
-                  }`}
-                >
-                  {trade.wonBet >= 0 ? (
-                    <>
-                      Growth
-                      {growthIcon}
-                    </>
-                  ) : (
-                    <>
-                      Loss
-                      {lossIcon}
-                    </>
-                  )}
+    <div className='p-px bg-gradient-to-br from-[#6A6A9B] to-[#100F1D] rounded-lg flex-grow -z-40'>
+      <div className='bg-primary rounded-lg text-textcolor p-4 md:p-6 flex-grow relative overflow-clip -z-20'>
+        <div class="absolute w-96 h-32 bg-accent blur-3xl rounded-full -z-10 opacity-20 -top-20 left-1/2 transform -translate-x-1/2"></div>
+        <div class="absolute w-32 h-32 bg-accent blur-3xl rounded-full -z-10 opacity-20 -bottom-20 left-0"></div>
+        <h2 className="md:text-sm tracking-wide pb-4 font-main">Recent Trades</h2>
+        <div className='h-64 overflow-y-scroll no-scrollbar'>
+          <ul className="divide-y divide-gray-300/5">
+            {salesData.map((trade, index) => (
+              <li key={index} className="py-4 flex items-center justify-between">
+                <div className="flex flex-row gap-1 items-center">
+                  <div className='w-fit h-fit rounded-full mr-2 bg-white'>{loadIcon(trade.tradeCurrency1)}</div>
+                  <p className="text-textcolor">
+                    {trade.tradeCurrency1}
+                  </p>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+                <div className="flex flex-col">
+                  <p className="text-right">
+                    {trade.wonBet >= 0 ? (
+                      <span>+${Math.abs(trade.wonBet)}</span>
+                    ) : (
+                      <span>-${Math.abs(trade.wonBet)}</span>
+                    )}
+                  </p>
+                  <div
+                    className={`text-white text-xs font-normal px-2 py-0.5 rounded mt-1 flex flex-row gap-1 items-center justify-center ${
+                      trade.wonBet >= 0 ? 'bg-[#3644DF]' : 'bg-[#EA3382]'
+                    }`}
+                  >
+                    {trade.wonBet >= 0 ? (
+                      <>
+                        Growth
+                        {growthIcon}
+                      </>
+                    ) : (
+                      <>
+                        Loss
+                        {lossIcon}
+                      </>
+                    )}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
