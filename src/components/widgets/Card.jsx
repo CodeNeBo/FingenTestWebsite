9886dd1f275a6c0576fd2c1e607486c5141988a6
Богдан Нечитайло <nechitaylob@gmail.com>
2024-04-08 16:52:30 +0300

    Added the right chart colors. Charts bug fix

--- src/components/widgets/Card.jsx
+++ src/components/widgets/Card.jsx
@@ -9,10 +9,13 @@ const Card = ({ title, currentNumber, lastMonthNumber, historyData }) => {
   const textColorClass = difference > 0 ? 'text-[#3644DF]' : 'text-[#EA3382]';
 
   const chartFill = currentNumber > lastMonthNumber ? '#3644DF' : '#EA3382';
+  const getChartGradient = () => {
+    return `url(#chartUv${currentNumber > lastMonthNumber ? 'Positive' : 'Negative'})`;
+  };
 
   return (
-    <div className='p-px bg-gradient-to-br from-[#6A6A9B] to-[#100F1D] rounded-lg overflow-clip w-full -z-40 relative h-full aspect-square md:aspect-auto'>
-      <div className="bg-primary text-textcolor p-4 md:p-5 relative overflow-hidden flex flex-col md:flex-row justify-between font-main w-full h-full rounded-[8px] md:rounded-[12px] lg:rounded-[9px]">
+    <div className='p-px bg-gradient-to-br from-[#6A6A9B] to-[#100F1D] rounded-lg overflow-clip w-full -z-40 relative h-full aspect-square sm:aspect-auto'>
+      <div className="bg-primary text-textcolor p-4 md:p-5 relative overflow-hidden flex flex-col md:flex-row justify-between font-main w-full h-full rounded-[8px] md:rounded-lg">
         <div className={`w-48 h-10 -rotate-45 ${bgColorClass} blur-2xl absolute -top-6 -right-0 md:-top-8 md:right-16 opacity-100 z-10`}></div>
         <div className={`md:hidden w-32 h-24 ${bgColorClass} blur-2xl absolute -bottom-16 -right-16 opacity-75 z-10`}></div>
         <div className='flex flex-col z-20 h-full'>
@@ -32,13 +35,18 @@ const Card = ({ title, currentNumber, lastMonthNumber, historyData }) => {
           <ResponsiveContainer width="100%" height={96}>
             <AreaChart data={historyData} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
               <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="100%">
-                  <stop offset="0%" stopColor="#8a14f6" stopOpacity={1.0} />
-                  <stop offset="30%" stopColor="#8a14f6" stopOpacity={0.9} />
-                  <stop offset="90%" stopColor="#8a14f6" stopOpacity={0.0} /> 
+                <linearGradient id="chartUvPositive" x1="0" y1="0" x2="0" y2="100%">
+                  <stop offset="0%" stopColor="#3644DF" stopOpacity={1.0} />
+                  <stop offset="30%" stopColor="#3644DF" stopOpacity={0.9} />
+                  <stop offset="90%" stopColor="#3644DF" stopOpacity={0.0} /> 
+                </linearGradient>
+                <linearGradient id="chartUvNegative" x1="0" y1="0" x2="0" y2="100%">
+                  <stop offset="0%" stopColor="#EA3382" stopOpacity={1.0} />
+                  <stop offset="30%" stopColor="#EA3382" stopOpacity={0.9} />
+                  <stop offset="90%" stopColor="#EA3382" stopOpacity={0.0} /> 
                 </linearGradient>
               </defs>
-              <Area type="monotone" dataKey="value" stroke="#8a14f6" fill="url(#colorUv)" />
+              <Area type="monotone" dataKey="value" stroke={chartFill} fill={getChartGradient()} />
             </AreaChart>
           </ResponsiveContainer>
         )}
