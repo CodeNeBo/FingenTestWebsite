28791fd60a9d278f73b439743f051dc993c61e50
Богдан Нечитайло <nechitaylob@gmail.com>
2025-02-07 09:58:30 +0200

    Update to astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,22 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import sitemap from '@astrojs/sitemap';
-// import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const whenExternalScripts = (items = []) => SITE.googleAnalyticsId ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-  output: 'static',
+  output: 'server',
+  adapter: netlify(),
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin]
   },
@@ -27,9 +26,7 @@ export default defineConfig({
       applyBaseStyles: false
     }
   }), 
-  // sitemap(), image({
-  //   serviceEntryPoint: '@astrojs/image/sharp'
-  // }), 
+
   mdx(), ...whenExternalScripts(() => partytown({
     config: {
       forward: ['dataLayer.push']
